3f06bfd8d50105696392f19c4cafb34a0f70ed72
Fernanda <guerragil.fernanda@gmail.com>
2019-11-28 02:37:00 +0100

    Ajax call and response ok with token.

--- comandos.js
+++ comandos.js
@@ -1,30 +1,35 @@
 function validate_inputPassword5(inputPassword5) {
     if (inputPassword5.value !== $("#inputPassword4").val()) {
-        inputPassword5.setCustomValidity("Duplicate passwords do not match");
+        inputPassword5.setCustomValidity("Passwords do not match");
     } else {
         inputPassword5.setCustomValidity(""); // is valid
     }
 }
 
+var http_request = new XMLHttpRequest();
 
 function petition() {
-
-    var http_request = new XMLHttpRequest(),
-        username = document.getElementById("userlogin").value,
-        password = document.getElementById("passlogin").value,
-        url = encodeURIComponent( "http://fenw.etsisi.upm.es:10000/users/login+?username=${username}&password=${password}");
+    var url = "http://fenw.etsisi.upm.es:10000/users/login";
+    var elUser = document.getElementById("userlogin").value;
+    var laPass = document.getElementById("passlogin").value;
+    url = url + "?username=" + elUser + "&password=" + laPass;
+    url = encodeURI(url);
     http_request.open("GET", url, true);
-    http_request.responseType='json';
+    http_request.responseType = 'json';
     http_request.onload = TrataRespuesta;
-    http_request.send(null);
+    http_request.send();
+}
 
-    function TrataRespuesta() {
+function TrataRespuesta() {
         var respuesta;
         if (http_request.status == 200) {
             respuesta = http_request.response;
-            document.getElementById("test").value = respuesta;
         } else {
             alert("Error con la URL");
         }
-    }
 }
+
+
+
+
+
